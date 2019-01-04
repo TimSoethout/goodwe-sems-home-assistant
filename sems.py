fbb637c56a54798ce586204240a6f5b376f0d08a
Tim Soethout <git@timsoethout.nl>
2019-01-04 22:25:06 +0100

    cleanup code

--- sems.py
+++ sems.py
@@ -1,19 +1,15 @@
-#!/usr/bin/env python3
-import voluptuous as vol
-
 import json
 import logging
-# import urllib.request
 import requests
+
+import voluptuous as vol
+
 from bs4 import BeautifulSoup
 
-from homeassistant.const import TEMP_CELSIUS
 from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD
 import homeassistant.helpers.config_validation as cv
-
-from homeassistant.helpers.entity import Entity
 
 REQUIREMENTS = ['BeautifulSoup4==4.7.0']
 
@@ -64,13 +60,13 @@ class SemsSensor(Entity):
             login_data = dict(account=self._config.get(CONF_USERNAME), pwd=self._config.get(CONF_PASSWORD))
 
             session_requests = requests.session()
-            r = session_requests.post(_URL, data=login_data)
+            response = session_requests.post(_URL, data=login_data)
 
-            htmlresponse = json.loads(r.text).get('data').get('redirect') # Convert response to dict
+            htmlresponse = json.loads(response.text).get('data').get('redirect') # Convert response to dict
             url = "https://www.semsportal.com" + htmlresponse
 
-            result = session_requests.get(url, headers = dict(referer = url) )
-            htmlcontent= result.content # transform the result to html content
+            result = session_requests.get(url, headers=dict(referer=url))
+            htmlcontent = result.content # transform the result to html content
 
             _LOGGER.debug("HTML content received")
 
@@ -79,23 +75,18 @@ class SemsSensor(Entity):
             _LOGGER.debug("HTML content parsed")
             _LOGGER.debug("update finished. result=%s", soup)
 
-            
             # Filtering
-            data = soup.find_all("script")[19].string
+            # data = soup.find_all("script")[19].string
             filter1 = str(soup).split("var pw_info = ")[1]
             filter2 = str(filter1).split("var pw_id = ")[0]
             filter3 = filter2.split(";")[0]
 
             filter4 = json.loads(filter3)
-            filter5 = dict(filter4)
+            # filter5 = dict(filter4)
 
-
-            for k, v in filter4['inverter'][0]['invert_full'].items():
-                # payload_dict = {}
-                key = k
-                value = v
+            for key, value in filter4['inverter'][0]['invert_full'].items():
                 self._attributes[key] = value
                 _LOGGER.debug("Updated attribute %s: %s", key, value)
-        except Exception as e:
+        except Exception as exception:
             _LOGGER.error(
-                "Unable to fetch data from SEMS. %s", e)
+                "Unable to fetch data from SEMS. %s", exception)
