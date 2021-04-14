133ca99d05f8dd169931342ac289de10fd48c27b
Tim Soethout <git@timsoethout.nl>
2021-04-14 22:38:52 +0200

    rename station to power station

--- custom_components/sems/const.py
+++ custom_components/sems/const.py
@@ -3,12 +3,20 @@
 DOMAIN = "sems"
 
 import voluptuous as vol
-from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, DEVICE_CLASS_POWER, POWER_WATT
-CONF_STATION_ID = "station_id"
+from homeassistant.const import (
+    CONF_PASSWORD,
+    CONF_USERNAME,
+    DEVICE_CLASS_POWER,
+    POWER_WATT,
+)
+
+CONF_STATION_ID = "powerstation_id"
 
 # Validation of the user's configuration
-SEMS_CONFIG_SCHEMA = vol.Schema({
-    vol.Required(CONF_USERNAME): str,
-    vol.Required(CONF_PASSWORD): str,
-    vol.Required(CONF_STATION_ID): str
-})
+SEMS_CONFIG_SCHEMA = vol.Schema(
+    {
+        vol.Required(CONF_USERNAME): str,
+        vol.Required(CONF_PASSWORD): str,
+        vol.Required(CONF_STATION_ID): str,
+    }
+)
