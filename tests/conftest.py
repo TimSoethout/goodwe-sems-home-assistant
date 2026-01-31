b8ae26ff2166d5757bb6f23011aa679ad2deaec5
Tim Soethout <git@timsoethout.nl>
2026-01-31 13:06:06 +0100

    no plugins for CI?

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@
 # Some CI environments set `PYTEST_DISABLE_PLUGIN_AUTOLOAD=1`, which prevents
 # entrypoint-based plugin discovery. Explicit loading keeps fixtures like
 # `hass` and `requests_mock` available everywhere.
-pytest_plugins = [
-    # "pytest_asyncio.plugin",
-    # "pytest_homeassistant_custom_component.plugins",
-    "requests_mock.contrib._pytest_plugin",
-]
+# pytest_plugins = [
+#     # "pytest_asyncio.plugin",
+#     # "pytest_homeassistant_custom_component.plugins",
+#     "requests_mock.contrib._pytest_plugin",
+# ]
